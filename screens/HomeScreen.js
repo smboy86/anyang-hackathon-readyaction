8c6c05a4218ed6eb9c75f45747b807f74ee049f9
smboy86 <smboy86@naver.com>
2019-11-01 00:26:11 +0900

    1차 개발(로그인, 리스트)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,4 @@
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import React from "react";
 import {
   Image,
   Platform,
@@ -8,191 +7,178 @@ import {
   Text,
   TouchableOpacity,
   View,
-} from 'react-native';
+  FlatList
+} from "react-native";
 
-import { MonoText } from '../components/StyledText';
+import { List, ListItem, Card } from "react-native-elements";
+import { randomUsers } from "./utils";
+import styled from "styled-components";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+
+// 여자 사진
+const testImg =
+  "https://d26oc3sg82pgk3.cloudfront.net/files/media/uploads/casting_call/b3bc4266-10f3-45b2-8c38-ad7a78df0528.jpg";
+
+const InputBox = styled.View`
+  height: 48px;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const SearchTextInput = styled.TextInput`
+  width: 100%;
+  height: 48px;
+  border: 1px solid #e6e6e6;
+  padding-left: 18px;
+  border-radius: 16px;
+  background-color: #fff;
+`;
+
+const SearchIconBox = styled.TouchableOpacity`
+  position: absolute;
+  top: 10px;
+  right: 10%;
+`;
+
+const ContainerCard = styled.TouchableOpacity`
+  flex: 1;
+  padding: 10px 20px 10px 20px;
+  background-color: #fff;
+  border-radius: 16px;
+  /* margin-top: 10px; */
+`;
+
+const ProfileBox = styled.View`
+  position: relative;
+`;
+
+const ProfileImg = styled.Image`
+  border-radius: 10px;
+  width: 100%;
+  height: 230px;
+`;
+
+const ProfileInfoBox = styled.View`
+  height: 80px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const PInfoLeftBox = styled.View`
+  width: 60%;
+`;
+
+const PInfoName = styled.Text`
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const PInfoTagDetailBox = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 4px;
+`;
+const PInfoLeftDetail = styled.TouchableOpacity`
+  padding: 5px;
+  /* border: 1px solid #e2e2e2; */
+  border: 1px solid #e2e2e2;
+  border-radius: 20px;
+  margin-right: 6px;
+`;
+
+const PInfoLeftText = styled.Text`
+  font-size: 12px;
+`;
+
+const PInfoRightBox = styled.View`
+  width: 40%;
+`;
 
 export default function HomeScreen() {
+  state = {
+    refreshing: false,
+    data: randomUsers(20)
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
+  renderItem = ({ item }) => (
+    <ContainerCard>
+      <ProfileBox>
+        <ProfileImg source={{ uri: item.avatar }} />
+        {/* <ProfileImg source={{ uri: testImg }} /> */}
+      </ProfileBox>
+      <ProfileInfoBox>
+        <PInfoLeftBox>
+          <PInfoName>{item.name}</PInfoName>
+          <PInfoTagDetailBox>
+            <PInfoLeftDetail>
+              <PInfoLeftText>185cm</PInfoLeftText>
+            </PInfoLeftDetail>
+            <PInfoLeftDetail>
+              <PInfoLeftText>79kg</PInfoLeftText>
+            </PInfoLeftDetail>
+            <PInfoLeftDetail>
+              <PInfoLeftText>근육형</PInfoLeftText>
+            </PInfoLeftDetail>
+          </PInfoTagDetailBox>
+        </PInfoLeftBox>
+        <PInfoRightBox>
+          <PInfoTagDetailBox>
+            <PInfoLeftDetail color={"red"}>
+              <PInfoLeftText>보조출연자</PInfoLeftText>
+            </PInfoLeftDetail>
+            <PInfoLeftDetail color={"blue"}>
+              <PInfoLeftText>사극</PInfoLeftText>
+            </PInfoLeftDetail>
+            <PInfoLeftDetail color={"blue"}>
+              <PInfoLeftText>현대극</PInfoLeftText>
+            </PInfoLeftDetail>
+          </PInfoTagDetailBox>
+        </PInfoRightBox>
+      </ProfileInfoBox>
+    </ContainerCard>
+  );
+
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
+    <ScrollView
+      style={{
+        flex: 1,
+        paddingTop: 16,
+        paddingHorizontal: 10,
+        backgroundColor: "#e2e2e2"
+      }}
+    >
+      <InputBox>
+        <SearchTextInput
+          onChangeText={text => text}
+          placeholder="포졸 연기자"
+          maxLength={18}
+          onSubmitEditing={({ nativeEvent: { text } }) => console.log(text)}
+          // onChangeText={text => onChangeSearchText(text)}
+          // value={}
+        ></SearchTextInput>
+        <SearchIconBox onPress={() => {}}>
+          <Ionicons name="md-search" size={26} color="black" />
+        </SearchIconBox>
+      </InputBox>
+      <FlatList
+        style={{ marginTop: 20 }}
+        keyExtractor={this.keyExtractor}
+        data={randomUsers(20)}
+        renderItem={this.renderItem}
+      />
+    </ScrollView>
   );
 }
 
 HomeScreen.navigationOptions = {
-  header: null,
+  // header: null
+  title: "레디액션",
+  headerRight: (
+    <View style={{ paddingRight: 10 }}>
+      <AntDesign name="filter" size={24} color="black" />
+    </View>
+  )
 };
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
